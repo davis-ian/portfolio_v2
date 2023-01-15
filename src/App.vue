29b85d0f161b90843f4cfa5c8bc2b61ed771e79b
iandavis <iandavis6481@gmail.com>
2023-01-15 09:20:58 -0600

    snackbar + favicon

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
 <template>
-  <router-view />
+  <v-app>
+    <v-main>
+      <v-app-bar app>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <!-- <v-app-bar-title>Ian</v-app-bar-title> -->
+      </v-app-bar>
+      <router-view />
+      <snackbar />
+    </v-main>
+  </v-app>
 </template>
 
-<script setup>
-  //
+<script>
+import Snackbar from "@/components/ui/Snackbar.vue";
+import { useSnackbarStore } from "./store/snackbar";
+
+export default {
+  setup() {
+    const snackbarStore = useSnackbarStore();
+    return { snackbarStore };
+  },
+  data() {
+    return {
+      menuShowing: false,
+    };
+  },
+  components: { Snackbar },
+};
 </script>
---
+++ src/components/ui/Snackbar.vue
@@ -0,0 +1,50 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div name="snackbars">
+    <v-snackbar v-model="show" :color="color" :timeout="timeout">
+      {{ message }}
+      <template v-if="button" v-slot:action="{ attrs }">
+        <v-btn
+          :color="button.color"
+          text
+          v-bind="attrs"
+          @click="button.callback"
+        >
+          {{ button.text }}
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+</template>
+
+<script>
+import { useSnackbarStore } from "@/store/snackbar";
+export default {
+  setup() {
+    const snackbarStore = useSnackbarStore();
+    return { snackbarStore };
+  },
+  created() {
+    this.snackbarStore.$onAction(({ name, after }) => {
+      // eslint-disable-next-line no-unused-vars
+      after((result) => {
+        if (name === "showSnackbar") {
+          this.message = this.snackbarStore.snackbar.message;
+          this.color = this.snackbarStore.snackbar.color;
+          this.timeout = this.snackbarStore.snackbar.timeout;
+          this.show = true;
+        }
+      });
+    });
+  },
+  data() {
+    return {
+      show: true,
+      color: "",
+      message: "",
+      timeout: null,
+      button: null,
+    };
+  },
+};
+</script>
