90fd609fceb28c82495113b790721679e5d64809
ian <iandavis6481@gmail.com>
2024-03-21 07:03:29 -0500

    console cleanup

--- src/utlities/utils.js
+++ src/utlities/utils.js
@@ -10,13 +10,9 @@ export const openEmail = function () {
 
 export function setLocalLightModeSettings(val) {
   localStorage.setItem("id-light-mode", val);
-
-  console.log("setting  light mode local storage", val);
 }
 
 export function getLocalLightModeSettings() {
   const currentMode = localStorage.getItem("id-light-mode");
-  console.log(currentMode, "light mode local stoarge val");
-
   return currentMode === "true" ?? false;
 }
